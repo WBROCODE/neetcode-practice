f386ad39f3ae19dfc5e447a1769054b77d109e18
GEZI Pro <110591410+WBROCODE@users.noreply.github.com>
2026-03-31 23:45:17 -0700

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        // SC: M * N * log M TC: N * M
+        // M : longest len of str, n: len of strs
+        Map<String, List<String>> map = new HashMap<>();
+        for(String str : strs){
+            int[]counts = new int[26];
+            for(char ch : str.toCharArray()){
+                counts[ch -'a']++;
+            }
+            String key = "";
+            for(int c : counts){
+                key += c + ",";
+            }
+            
+            map.computeIfAbsent(key, k->new ArrayList<>()).add(str);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+}
