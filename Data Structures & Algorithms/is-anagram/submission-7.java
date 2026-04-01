b24bba6295886fbb1c85dfd67e1296fc0a63f305
GEZI Pro <110591410+WBROCODE@users.noreply.github.com>
2026-03-31 22:42:45 -0700

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[]map = new int[26];
+
+        for(char ch : s.toCharArray()){
+            map[ch - 'a']++;
+        }
+
+        for(char ch : t.toCharArray()){
+            map[ch - 'a']--;
+        }
+        for(int a : map){
+            if(a != 0) return false;
+        }
+        return true;
+    }
+}
