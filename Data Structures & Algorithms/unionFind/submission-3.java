3207731537a7bb30caf301a5204f08bbbb243839
GEZI Pro <110591410+WBROCODE@users.noreply.github.com>
2026-03-31 22:16:37 -0700

    Add: unionFind - submission-3

---
+++ Data Structures & Algorithms/unionFind/submission-3.java
@@ -0,0 +1,46 @@
+class UnionFind {
+    int[] parents;
+    int[] ranks;
+    int size;
+    public UnionFind(int n) {
+        this.size = n;
+        this.parents = new int[n];
+        this.ranks = new int[n];
+
+        for(int i = 0; i < n; i++){
+            parents[i] = i;
+            ranks[i] = 1;
+        }
+    }
+
+    public int find(int x) {
+        if(parents[x] != x) parents[x] = find(parents[x]);
+        return parents[x];
+    }
+
+    public boolean isSameComponent(int x, int y) {
+        int pX = find(x), pY = find(y);
+        if(pX == pY) return true;
+
+        return false;
+    }
+
+    public boolean union(int x, int y) {
+        int pX = find(x), pY = find(y);
+        if(pX == pY) return false;
+
+        if(ranks[pX] >= ranks[pY]){
+            parents[pY] = pX;
+        }else{
+            parents[pX] = pY;
+            ranks[pY]++;            
+        }
+        this.size--;
+
+        return true;
+    }
+
+    public int getNumComponents() {
+        return this.size;
+    }
+}
