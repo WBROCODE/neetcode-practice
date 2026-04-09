7a1aad4a904caba04d23874b5cf18ea11fb6903e
GEZI Pro <110591410+WBROCODE@users.noreply.github.com>
2026-04-08 22:43:32 -0700

    Add: binary-tree-maximum-path-sum - submission-2

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-2.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    int ans = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return ans;
+    }
+
+    public int dfs(TreeNode root){
+        if(root == null) return 0;
+
+        int leftM = Math.max(dfs(root.left),0);
+        int rightM = Math.max(dfs(root.right),0);
+        ans = Math.max(leftM + rightM + root.val, ans);
+        return Math.max(leftM, rightM) + root.val;
+    }
+}
