53f29f0deb8e460150a696a197c2a96eb2a6849b
GEZI Pro <110591410+WBROCODE@users.noreply.github.com>
2026-04-10 22:47:38 -0700

    Add: implement-prefix-tree - submission-5

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-5.java
@@ -0,0 +1,41 @@
+class PrefixTree {
+    TrieNode root;
+    public PrefixTree() {
+         root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode cur = this.root;
+        for(char ch : word.toCharArray()){
+            if(cur.children[ch] == null)cur.children[ch] = new TrieNode();
+            cur = cur.children[ch];
+        }
+        cur.isWord = true;
+
+    }
+
+    public boolean search(String word) {
+        TrieNode cur = this.root;
+        for(char ch : word.toCharArray()){
+            if( cur.children[ch] == null) return false;
+            cur = cur.children[ch];
+        }
+        return cur.isWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode cur = this.root;
+        for(char ch : prefix.toCharArray()){
+            if(cur.children[ch] == null) return false;
+            cur = cur.children[ch];
+        }
+        return true;
+    }
+}
+
+
+class TrieNode{
+    TrieNode[] children = new TrieNode[128];
+    boolean isWord;
+    public TrieNode(){}
+}
