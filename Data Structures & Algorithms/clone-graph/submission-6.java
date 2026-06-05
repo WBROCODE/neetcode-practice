d9adcbb3b0f6afee3bdc8626c64314684669b71b
GEZI Pro <110591410+WBROCODE@users.noreply.github.com>
2026-06-04 22:23:13 -0700

    Add: clone-graph - submission-6

---
+++ Data Structures & Algorithms/clone-graph/submission-6.java
@@ -0,0 +1,41 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    Map<Node, Node> map = new HashMap<>();
+    public Node cloneGraph(Node node) {
+        return dfs(node);
+    }
+
+    public Node dfs(Node node){
+        if(node == null) return node;
+        if(map.containsKey(node)) return map.get(node);
+
+        Node newN = new Node(node.val);
+        List<Node> children = newN.neighbors;
+        map.put(node, newN);
+        for(Node n : node.neighbors){
+            children.add(dfs(n));
+        }
+
+
+        return newN;
+    }
+}
