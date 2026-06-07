b8e6615f736a53f5be018c29053ba1a65199f055
GEZI Pro <110591410+WBROCODE@users.noreply.github.com>
2026-06-07 14:56:19 -0700

    Add: copy-linked-list-with-random-pointer - submission-3

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-3.java
@@ -0,0 +1,42 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if(head == null) return head;
+        Node cur = head;
+        Map<Node, Node> map = new HashMap<>();
+        while(cur != null){
+            map.put(cur, new Node(cur.val));
+            cur = cur.next;
+        }
+
+        cur = head;
+
+        while(cur != null){
+
+            if(cur.next != null){
+                map.get(cur).next = map.get(cur.next);
+            }
+
+            if(cur.random != null){
+                map.get(cur).random = map.get(cur.random);
+            }
+            cur = cur.next;
+        }
+
+        return map.get(head);
+    }
+}
