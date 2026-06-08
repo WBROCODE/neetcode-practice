b86ad6a87ef772af014e9e16b6bf545b54a365bc
GEZI Pro <110591410+WBROCODE@users.noreply.github.com>
2026-06-07 18:08:53 -0700

    Add: course-schedule - submission-9

---
+++ Data Structures & Algorithms/course-schedule/submission-9.java
@@ -0,0 +1,42 @@
+class Solution {
+    boolean[] visited;
+    boolean[] onPath;
+    boolean hasCircle = false;
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        visited = new boolean[numCourses];
+        onPath = new boolean[numCourses];
+
+        List<Integer>[] graph = new ArrayList[numCourses];
+        for(int i = 0; i < numCourses; i++){
+            graph[i] = new ArrayList<>();
+        }
+        for(int[] prerequisite : prerequisites){
+            int from = prerequisite[1];
+            int to = prerequisite[0];
+            graph[from].add(to);
+        }
+
+        for(int i = 0; i < numCourses; i++){
+            dfs(graph, i);
+        }
+
+        for(int i = 0; i < numCourses; i++){
+            if(!visited[i]) return false;
+        }
+
+        return !hasCircle;
+    }
+
+    public void dfs(List<Integer>[] graph, int cur){
+        if(onPath[cur]){
+            hasCircle = true;
+            return;
+        }
+        visited[cur] = true;
+        onPath[cur] = true;
+        for(int next : graph[cur]){
+            dfs(graph, next);
+        }
+        onPath[cur] = false;
+    }
+}
