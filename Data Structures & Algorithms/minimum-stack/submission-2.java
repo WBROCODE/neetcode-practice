4cd12ad11330e89950c5c5558637f271776ade0a
GEZI Pro <110591410+WBROCODE@users.noreply.github.com>
2026-06-13 17:51:09 -0700

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.java
@@ -0,0 +1,37 @@
+class MinStack {
+    Stack<Integer> st;
+    Stack<Integer> min;
+    public MinStack() {
+        st = new Stack();
+        min = new Stack();
+    }
+    /**
+    
+        0
+        2
+        1       1
+        st      min
+    */
+    public void push(int val) {
+        st.push(val);
+        if(min.isEmpty() || min.peek() > val){
+            min.push(val);
+        }else{
+            min.push(min.peek());
+        }
+        
+    }
+    
+    public void pop() {
+        st.pop();
+        min.pop();
+    }
+    
+    public int top() {
+        return st.peek();
+    }
+    
+    public int getMin() {
+        return min.peek();
+    }
+}
