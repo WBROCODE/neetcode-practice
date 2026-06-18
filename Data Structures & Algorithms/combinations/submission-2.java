6effa3094d0f84be1bb0d17b6c8ed837f7fdb67c
GEZI Pro <110591410+WBROCODE@users.noreply.github.com>
2026-06-17 21:27:20 -0700

    Add: combinations - submission-2

---
+++ Data Structures & Algorithms/combinations/submission-2.java
@@ -0,0 +1,28 @@
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+
+    public List<List<Integer>> combine(int n, int k) {
+        List<Integer> temp = new ArrayList<>();
+        for(int i = 1; i <= n; i++){
+            temp.add(i);
+        }
+        backTrack(n, k, 1);
+        return ans;
+    }
+
+    public void backTrack(int n, int k, int start){
+        if(path.size() == k){
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+        if(start > n) return;
+        for(int i = start; i <= n; i++){
+            path.add(i);
+            backTrack(n, k, i + 1);
+            path.remove(path.size() - 1);
+        }
+
+
+    }
+}
