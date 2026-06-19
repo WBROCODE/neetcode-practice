d8772ac4d16067892071055df2a44616d1e32ad8
GEZI Pro <110591410+WBROCODE@users.noreply.github.com>
2026-06-18 21:28:11 -0700

    Add: sort-an-array - submission-2

---
+++ Data Structures & Algorithms/sort-an-array/submission-2.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        int n = nums.length;
+        quickSort(0, n - 1, nums);
+        return nums;
+    }
+
+    public void quickSort(int left, int right, int[]nums){
+        if(left >= right) return;
+        int pivot = nums[left + (right - left) / 2];
+        int l = left;
+        int r = right;
+
+        while(l <= r){
+            while(nums[l] < pivot){
+                l++;
+            }
+
+            while(nums[r] > pivot){
+                r--;
+            }
+
+            if(l <= r){
+                int temp = nums[l];
+                nums[l] = nums[r];
+                nums[r] = temp;
+                l++;
+                r--;
+            }
+        }
+
+        quickSort(left, r, nums);
+        quickSort(l, right, nums);
+    }
+}
