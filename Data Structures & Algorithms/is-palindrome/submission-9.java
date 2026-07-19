b63fb28d4dffe37e5034406e93c15c9591d27c23
GEZI Pro <110591410+WBROCODE@users.noreply.github.com>
2026-07-18 22:45:20 -0700

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        /**
+        
+            Was it a car or a cat I saw?
+
+
+            compare only character
+        */
+
+        int l = 0, r = s.length() - 1;
+
+        while(l < r){
+            char chL = s.charAt(l);
+            char chR = s.charAt(r);
+            if(Character.isLetterOrDigit(chL) && Character.isLetterOrDigit(chR)){
+                if(Character.toLowerCase(chL) != Character.toLowerCase(chR)) return false;
+                l++;
+                r--;
+            }else if(Character.isLetterOrDigit(chL)){
+                r--;
+            }else if(Character.isLetterOrDigit(chR)){
+                l++;
+            }else{
+                l++;
+                r--;
+            }
+        }
+
+        return true;
+    }
+}
