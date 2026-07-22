43b1d9c388bb70023b7c0228569d6817cfb12b86
GEZI Pro <110591410+WBROCODE@users.noreply.github.com>
2026-07-21 21:23:50 -0700

    Add: eating-bananas - submission-8

---
+++ Data Structures & Algorithms/eating-bananas/submission-8.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int l = 1, r = 0;
+        for(int p : piles){
+            r = Math.max(p, r);
+        }
+
+        while(l < r){
+            int m = l + (r - l) / 2;
+            if(check(m, piles, h)){
+                r = m;
+            }else l = m + 1;
+        }
+
+        return l;
+    }
+
+    public boolean check(int m, int[]piles, int h){
+        int time = 0;
+
+        for(int p : piles){
+            time += Math.ceil((double)p / m);
+            if(time > h) return false;
+        }
+        return time <= h;
+    }
+}
