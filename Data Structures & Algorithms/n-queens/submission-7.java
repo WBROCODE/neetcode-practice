6b5a3d2be184831d01fe676d8b3ea67b0e22e97a
GEZI Pro <110591410+WBROCODE@users.noreply.github.com>
2026-07-23 21:59:38 -0700

    Add: n-queens - submission-7

---
+++ Data Structures & Algorithms/n-queens/submission-7.java
@@ -0,0 +1,58 @@
+class Solution {
+    List<List<String>> ans;
+    char[][] board;
+
+    public List<List<String>> solveNQueens(int n) {
+        // place by col
+        ans = new ArrayList<>();
+        board = new char[n][n];
+        for(int i =0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                board[i][j] = '.';
+            }
+        }
+        backTrack(0, n);
+
+        return ans;
+    }
+
+    public void backTrack(int start, int n){
+        if(start == n){
+            // add
+            List<String> temp = new ArrayList<>();
+            for(int i = 0; i < n; i++){
+                temp.add(new String(board[i]));
+            }
+            ans.add(new ArrayList<>(temp));
+            return ;
+        }
+
+        for(int i = 0; i < n; i++){
+            if(valid(i, start, n)){
+                board[i][start] = 'Q'; 
+                backTrack(start + 1, n);
+                board[i][start] = '.'; 
+            }
+        }
+    }
+
+    public boolean valid(int x, int y, int n){
+        for(int i = 0; i < n; i++){
+            if(board[x][i] == 'Q') return false;
+        }
+        for(int i = 0; i < n; i++){
+            if(board[i][y] == 'Q') return false;
+        }
+
+        for(int i = x - 1, j = y - 1; i >= 0 && j >= 0; i--, j--){
+            if(board[i][j] == 'Q') return false;
+        }
+
+        for(int i = x + 1, j = y - 1; i < n && j >= 0 ; i++, j--){
+            if(board[i][j] == 'Q') return false;
+        }
+        return true;
+    }
+
+}
+
