460c873b52c563e0844a4d8384b488842e61d6cc
GEZI Pro <110591410+WBROCODE@users.noreply.github.com>
2026-07-23 21:33:48 -0700

    Add: palindrome-partitioning - submission-3

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-3.java
@@ -0,0 +1,36 @@
+class Solution {
+    List<List<String>> ans = new ArrayList<>();
+    List<String> path = new ArrayList<>();
+
+    public List<List<String>> partition(String s) {
+        backTrack(s, 0);
+        return ans;
+    }
+
+    public void backTrack(String s, int start){
+        if(s.length() == start){
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+        if(start > s.length()) return;
+        for(int i = start; i < s.length(); i++){
+            String sub = s.substring(start, i + 1);
+            if(isPalindrome(sub)){
+                path.add(sub);
+                backTrack(s, i + 1);
+                path.remove(path.size() - 1);
+            }
+        }
+
+    }
+
+    public boolean isPalindrome(String str){
+        int l = 0, r = str.length() - 1;
+
+        while(l <= r){
+            if(str.charAt(l++) != str.charAt(r--)) return false;
+        }
+
+        return true;
+    }
+}
