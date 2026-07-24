21ecfa4212070340100803ac69a8e7ab10369951
GEZI Pro <110591410+WBROCODE@users.noreply.github.com>
2026-07-23 21:00:15 -0700

    Add: subsets - submission-3

---
+++ Data Structures & Algorithms/subsets/submission-3.java
@@ -0,0 +1,21 @@
+class Solution {
+    List<List<Integer>> ans;
+    List<Integer> path;
+    public List<List<Integer>> subsets(int[] nums) {
+        ans = new ArrayList<>();
+        path = new ArrayList<>();
+        int n = nums.length;
+        bT(nums, 0);
+        return ans;
+    }
+
+    public void bT(int[]nums, int start){
+        ans.add(new ArrayList<>(path));
+
+        for(int i = start; i < nums.length; i++){
+            path.add(nums[i]);
+            bT(nums, i + 1);
+            path.remove(path.size() - 1);
+        }
+    }
+}
