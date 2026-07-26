11584e6db2f12013e17719603bf32b95e29fdbb5
GEZI Pro <110591410+WBROCODE@users.noreply.github.com>
2026-07-26 15:48:16 -0700

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.java
@@ -0,0 +1,18 @@
+class Solution {
+    int[]memo;
+    public int climbStairs(int n) {
+        memo = new int[n + 1];
+        Arrays.fill(memo, -1);
+        return dp(n);
+    }
+
+    public int dp(int n){
+        if(n <= 2) return n;
+
+        if(memo[n] != -1 )return memo[n];
+
+        memo[n] = dp(n - 1) + dp(n - 2);
+
+        return memo[n];
+    }
+}
