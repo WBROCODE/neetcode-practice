d00c2c479b1351b5c03f848082e4baffd27f9a2e
GEZI Pro <110591410+WBROCODE@users.noreply.github.com>
2026-07-26 18:02:15 -0700

    Add: longest-palindromic-substring - submission-8

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-8.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        /**
+        
+        
+            ababd
+               i
+             j
+        
+        */
+        int n = s.length();
+        int start = 0, len = 0;
+        boolean[][] dp = new boolean[n][n];
+
+        for(int i = 0; i < n; i++){
+            for(int j = i; j >= 0; j--){
+                if(s.charAt(i) == s.charAt(j)){
+                    if(i - j + 1 < 3){
+                        dp[i][j] = true;
+                    }else{
+                        dp[i][j] = dp[i - 1][j + 1];
+                        
+                    }
+                }
+                if(i - j + 1 > len && dp[i][j]){
+                    len = i - j + 1;
+                    start = j;
+                }
+            }
+        }
+        return s.substring(start, start + len);
+    }
+}
