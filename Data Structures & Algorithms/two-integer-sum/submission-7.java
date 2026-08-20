00d383cab4eb8589c1577b04dbef7d03e33bef76
GEZI Pro <110591410+WBROCODE@users.noreply.github.com>
2026-08-19 17:35:01 -0700

    Update: two-integer-sum - submission-7

--- Data Structures & Algorithms/two-integer-sum/submission-7.java
+++ Data Structures & Algorithms/two-integer-sum/submission-7.java
@@ -1,15 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        /**       
+            remain = target - nums[i]
+        */
         int n = nums.length;
         Map<Integer, Integer> map = new HashMap<>();
-
         for(int i = 0; i < n; i++){
             int remain = target - nums[i];
-
-            if(map.containsKey(remain)){
-                return new int[]{map.get(remain), i};
-            }
-
+            if(map.containsKey(remain)) return new int[]{map.get(remain), i};
             map.put(nums[i], i);
         }
         return new int[]{};
