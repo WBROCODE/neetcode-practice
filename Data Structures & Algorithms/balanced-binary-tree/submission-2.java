2dec7f1e9083fa3847d8fcbeba7eaf4a45adb8f5
GEZI Pro <110591410+WBROCODE@users.noreply.github.com>
2026-08-24 13:39:59 -0700

    Add: balanced-binary-tree - submission-2

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-2.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    boolean isBalance = true;
+    public boolean isBalanced(TreeNode root) {
+        maxDepth(root);
+        return isBalance;
+    }
+
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        if(Math.abs(left - right) > 1) isBalance = false;
+        return Math.max(left, right) + 1;
+    }
+}
