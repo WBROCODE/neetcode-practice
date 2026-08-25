5522aaaf28e5ad6295d75b2efaab8e63c60c543e
GEZI Pro <110591410+WBROCODE@users.noreply.github.com>
2026-08-25 15:22:12 -0700

    Add: kth-largest-integer-in-a-stream - submission-4

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-4.java
@@ -0,0 +1,22 @@
+class KthLargest {
+    PriorityQueue<Integer> pq;
+    int k;
+    public KthLargest(int k, int[] nums) {
+        pq = new PriorityQueue<>((a,b)->a - b);
+        for(int n : nums){
+            pq.offer(n);
+            if(pq.size() > k){
+                pq.poll();
+            }
+        }
+        this.k = k;
+    }
+    
+    public int add(int val) {
+        pq.offer(val);
+        if(pq.size() > k){
+            pq.poll();
+        }
+        return pq.peek();
+    }
+}
