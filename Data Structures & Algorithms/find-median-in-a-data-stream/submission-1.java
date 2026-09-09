024bba6a0fbd702b4385338c24beae54b8f4abb2
GEZI Pro <110591410+WBROCODE@users.noreply.github.com>
2026-09-09 13:44:18 -0700

    Add: find-median-in-a-data-stream - submission-1

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-1.java
@@ -0,0 +1,24 @@
+class MedianFinder {
+    PriorityQueue<Integer> min = new PriorityQueue<>();
+    PriorityQueue<Integer> max = new PriorityQueue<>((a,b) -> b - a);
+    public MedianFinder() {
+        /**        
+            3
+            1
+        */
+    }
+    
+    public void addNum(int num) {
+        if(min.size() >= max.size()){
+            min.offer(num);
+            max.offer(min.poll());
+        }else{
+            max.offer(num);
+            min.offer(max.poll());
+        }
+    }
+    
+    public double findMedian() {    
+        return min.size() == max.size()? (double)(min.peek() + max.peek()) / 2.0 : (double)  max.peek();
+    }
+}
